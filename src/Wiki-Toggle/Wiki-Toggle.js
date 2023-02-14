6d5a6a36d60fcf4f92b9a8f83418939e492ac04b
dilipadavath <adavath.dilip@indegene.com>
2023-02-14 12:18:28 +0530

    added bootstrap

---
+++ src/Wiki-Toggle/Wiki-Toggle.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function WikiToggle() {
+    return (
+      <div className="WikiToggle">
+            <div className="container">Hi</div>
+      </div>
+    );
+  }
+  
+  export default WikiToggle;
